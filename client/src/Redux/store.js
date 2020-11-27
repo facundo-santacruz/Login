9fd3678bdcb6ecbd9e654975e15b2caa33817317
facundo-santacruz <facugs2090@gmail.com>
2020-11-27 15:11:18 -0300

    creando carpeta redux y el store

---
+++ client/src/Redux/store.js
@@ -0,0 +1,21 @@
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducer/index.js";
+
+// const middleware = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+// export default store;
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from "./reducer/index.js";
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+
+export default store;
